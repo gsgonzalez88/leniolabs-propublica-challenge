87eabc44f02bf3765ac879576fff76a6c0105594
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2021-12-04 23:45:45 -0300

    added routing and home page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'leniolabs-propublica-challenge'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('leniolabs-propublica-challenge');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('leniolabs-propublica-challenge app is running!');
-  });
 });
