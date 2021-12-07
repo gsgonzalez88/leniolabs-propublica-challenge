1fa75343549a1b9e5bcaf798126eced0cc439af4
Gerardo Gonzalez <gerardo.gonzalez@naranja.com>
2021-12-07 01:09:49 -0300

    adding methods for searching

--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -1,19 +1,27 @@
+import { MembersService } from './../../shared/services/members.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.page';
+import { of } from 'rxjs';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let memberServiceMock: any;
+
+  memberServiceMock = jasmine.createSpyObj('MembersService', ['getMemberList']);
+  memberServiceMock.getMemberList.and.returnValue(of([{}]));
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [{ provide: MembersService, useValue: memberServiceMock }]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,3 +1,4 @@
+import { MembersService } from './../../shared/services/members.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,10 +6,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.css']
 })
 export class HomeComponent implements OnInit {
+  members = [];
+  data: any[] = [];
+  pageReady = false;
+  found: any[] = [];
 
-  constructor() { }
+  constructor(
+    private membersService: MembersService
+  ) { }
 
   ngOnInit(): void {
+    this.setMemberList();
+  }
+
+  setMemberList(): void {
+    this.membersService.getMemberList()
+    .subscribe((list) => {
+      this.members = list.results[0].members;
+      this.data = this.members.map((member: any) => {
+        return {
+          'name': member.first_name,
+          'last_name': member.last_name,
+          'gender': member.gender,
+          'party': member.party,
+        }
+      })
+      this.pageReady = true;
+    })
+  }
+
+  search(info: any): void {
+    this.found = [];
+    this.data.forEach((member: any) => {
+      if(info.filter === 'global') {
+        this.searchGlobal(info.value, member);
+      } else {
+        if(member[info.filter] == info.value) {
+          this.found.push(member);
+        }
+      }
+    })
+    if(this.found.length === 0) {
+      this.setMemberList();
+    }
+    this.data = this.found;
+  }
+
+  searchGlobal(value: string, member: any): void {
+    Object.keys(member).forEach((key) => {
+      if(member[key] == value) {
+        this.found.push(member);
+      }
+    })
   }
 
 }
